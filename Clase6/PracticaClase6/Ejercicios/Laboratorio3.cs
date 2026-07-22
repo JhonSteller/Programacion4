ae1e32e8406ae0f8bfaa38f3bb43dacbb3b2327b
JhonSteller <jsteller23@gmail.com>
2026-07-21 19:32:55 -0600

    Create Laboratorio3.cs

---
+++ Clase6/PracticaClase6/Ejercicios/Laboratorio3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaClase6.Ejercicios
+{
+    internal class Laboratorio3
+    {
+    }
+}
